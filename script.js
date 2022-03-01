2475015e322625d621b7f446fb30636738290030
kyrotomato <97704480+kyrotomato@users.noreply.github.com>
2022-03-01 10:02:01 -0600

    added dropdown and preventdefault code

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+formEl.addEventListener("submit", createTaskHandler);
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
